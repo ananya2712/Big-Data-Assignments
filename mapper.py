a9ab99aa628ec65b138518185457fda42652697d
Ananya Uppal <55040997+ananya2712@users.noreply.github.com>
2021-09-11 19:09:52 +0530

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import json
+
+output = {}
+for line in sys.stdin:
+	data = json.loads(line)
+	if (data['severity']>=2 and data['sunrise_sunset']=='Night' and data['Visibility(mi)'] <= 10 and data['Precipitation(in)'] > 0.2 ):
+	if((data['Weather_Condition') in ["Heavy Snow", "Thunderstorm", "Heavy Rain", "Heavy Rain Showers", "Blowing Dust"]):
+	if("lane blocked" in data['Description'] or "shoulder blocked" in data['Description'] or "overturned vehicle" in data['Description']):
+	
+	 
+		
+	
+		
+	
+	
+	
+	
