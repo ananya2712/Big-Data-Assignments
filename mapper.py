cf6255cb33f4ae3b6acee7ba1d8c73c1acdad31d
Ananya Uppal <55040997+ananya2712@users.noreply.github.com>
2021-09-12 10:49:46 +0530

    Add files via upload

--- mapper.py
+++ mapper.py
@@ -3,13 +3,16 @@
 import json
 import sys
 
+count=0
 output = {}
 for line in sys.stdin:
 	data = json.loads(line)
-	if (data['Severity']>=2 and data['Sunrise_Sunset']=='Night' and data['Visibility(mi)'] <= 10 and data['Precipitation(in)'] > 0.2 ):
+	if (data['Severity']>=2 and data['Sunrise_Sunset']=='Night' and data['Visibility(mi)'] <= 10 and data['Precipitation(in)'] >=0.2 ):
 		if(data['Weather_Condition'] in ["Heavy Snow", "Thunderstorm", "Heavy Rain", "Heavy Rain Showers", "Blowing Dust"]):
 			if("lane blocked" in data['Description'] or "shoulder blocked" in data['Description'] or "overturned vehicle" in data['Description']):
 				print(line)
+				count=count+1
+print(count)
 	
 	
 	 
--- reducer.py
+++ reducer.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 import datetime
-import sys 
+import sys
+import json 
+import ast
 
 output = {}
 
-for line in sys.stdin:
-	#line = line.strip()		
+for line in sys.stdin:	
 	
-	'''hour = line['Start_time'].datetime.hour
+	"""hour = line['Start_time'].datetime.hour
 	if hour not in output.keys():
 		output[hour] = 1
 	else:
-		output[hour] = output[hour] + 1''' 
+		output[hour] = output[hour] + 1"""
 		
-	print(line)
+print(output)
 	
 	
 	
