58c0ab17294e3f554db1de1445130b0f498da149
Ananya Uppal <55040997+ananya2712@users.noreply.github.com>
2021-09-12 12:37:12 +0530

    Add files via upload

--- mapper.py
+++ mapper.py
@@ -4,15 +4,16 @@ import json
 import sys
 
 count=0
-output = {}
+
 for line in sys.stdin:
 	data = json.loads(line)
-	if (data['Severity']>=2 and data['Sunrise_Sunset']=='Night' and data['Visibility(mi)'] <= 10 and data['Precipitation(in)'] >=0.2 ):
+	if (data['Severity']>=2 and data['Sunrise_Sunset']=='Night' and data['Visibility(mi)'] <= 10.0 and data['Precipitation(in)'] >=0.2 ):
 		if(data['Weather_Condition'] in ["Heavy Snow", "Thunderstorm", "Heavy Rain", "Heavy Rain Showers", "Blowing Dust"]):
-			if("lane blocked" in data['Description'] or "shoulder blocked" in data['Description'] or "overturned vehicle" in data['Description']):
-				print(line)
-				count=count+1
-print(count)
+			if("lane blocked" in data['Description'].lower() or "shoulder blocked" in data['Description'].lower() or "overturned vehicle" in data['Description'].lower()):
+				print(data['Start_Time'])
+				#print(data)
+				#count=count+1
+#print(count)
 	
 	
 	 
--- reducer.py
+++ reducer.py
@@ -8,14 +8,18 @@ import ast
 output = {}
 
 for line in sys.stdin:	
-	
-	"""hour = line['Start_time'].datetime.hour
-	if hour not in output.keys():
-		output[hour] = 1
-	else:
-		output[hour] = output[hour] + 1"""
-		
-print(output)
+	line=line.strip()
+	words = line.split()
+	for word in words:
+		if '-' not in word:
+			time = word.split(':')
+			if time[0] not in output.keys():
+				output[time[0]] = 1
+			else:
+				output[time[0]] = output[time[0]] + 1
+				
+for i in sorted(output):
+    print (i, output[i])
 	
 	
 	
