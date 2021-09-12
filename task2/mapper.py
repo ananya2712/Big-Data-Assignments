1369bbf31782af6ea278384919cdcd4ad8649264
Ananya Uppal <55040997+ananya2712@users.noreply.github.com>
2021-09-12 18:23:52 +0530

    Add files via upload

--- task2/mapper.py
+++ task2/mapper.py
@@ -5,24 +5,27 @@ import json
 import math
 import requests
 
-latitude = sys.argv[1]
-longitude = sys.argv[2]
-d = sys.argv[3]
+latitude = float(sys.argv[1])
+longitude = float(sys.argv[2])
+d = float(sys.argv[3])
 
 def euclid_dist(line):
-	 dist = (latitude-line['Start_Lat'])**2 +(longitude-line['Start_Lng'])**2
+	 dist = (latitude-float(line['Start_Lat']))**2 +(longitude-float(line['Start_Lng']))**2
 	 dist = dist**0.5
 	 return dist
 	 
 for line in sys.stdin:
 	line = json.loads(line)
-	dist = euclid_dist(line)
-	if dist > d:
-		continue
-	else:
-		payload = {"latitude": line['Start_Lat'],"longitude":line['Start_Lng']}
-		response = requests.post(url = "http://20.185.44.219:5000/", data = payload)
-		print(response)
+	
+	if line['Start_Lat'] and line['Start_Lng']:
+		dist = euclid_dist(line)
+		if dist > d:
+			continue
+		else:
+			payload = {"latitude": line['Start_Lat'],"longitude":line['Start_Lng']}
+			#print(type(payload))
+			response = requests.post(url = "http://20.185.44.219:5000/", json = payload)
+			print(response.json()['city'],response.json()['state'])
 		
 	
 
