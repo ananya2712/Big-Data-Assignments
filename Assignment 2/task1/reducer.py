4fda52c99b759ba56eb3a45bad190ea91221b4bf
Ananya Uppal <55040997+ananya2712@users.noreply.github.com>
2021-10-19 18:07:47 +0530

    Add files via upload

--- Assignment 2/task1/reducer.py
+++ Assignment 2/task1/reducer.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
 
 import sys
+import string 
 
+prev_src = None
+filename = sys.argv[1]
+f = open(filename,'a')
+f.truncate(0)
 
 adj_list = []
 curr_src = None
-
-f = open(sys.argv[1], "a")
 
 for line in sys.stdin:
 	line = line.strip()
 	data = line.split('\t')
 	
-	# print(data)
-	
 	src = data[0]
 	dest = data[1]
 	
-	# print(dest)
-		
-	
 	if curr_src == None:
 		curr_src = src
-		# print('curr src:', curr_src)
-	
-	# print(src, dest)
+		f.write(src+','+str(1)+'\n')
 	
 	if curr_src == src and src != None:
 		adj_list.append(dest)
 		
 	elif curr_src != src and src != None:
-	
+		f.write(src+','+str(1)+'\n')
 		print(curr_src,'\t', "[", end="")
 		for node in range(len(adj_list)):
 			if(node == len(adj_list)-1):
@@ -59,3 +55,14 @@ for node in range(len(adj_list)):
 	else:
 		print(adj_list[node], ",", end=" ")
 print("]")
+
+f.close()
+records = list()
+#filename = 'v.txt'
+with open(filename) as fin:
+	for line in fin:
+		records.append(line.strip())
+records.sort()
+with open(filename,'w') as fout:
+	for line in records: 
+		fout.write(line + '\n')
