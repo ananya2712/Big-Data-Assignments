25b2d160fb77d9a7f2dad538c9619beb83e3f034
Ananya Uppal <55040997+ananya2712@users.noreply.github.com>
2021-10-21 10:41:42 +0530

    Add files via upload

--- Assignment 2/task2/reducer.py
+++ Assignment 2/task2/reducer.py
@@ -6,6 +6,10 @@
 
 import sys
 
+filename = sys.argv[1]
+f = open(filename,'a')
+f.truncate(0)
+
 curr_dest = None
 curr_contrib = 0.0
 
@@ -17,10 +21,13 @@ for line in sys.stdin:
 	else:
 		if curr_dest is not None:
 			rank = 0.15 + 0.85*curr_contrib
-			print(curr_dest,round(rank,2),sep=",")
+			#print(curr_dest,round(rank,2),sep=",")
+			f.write(curr_dest+','+str(round(rank,2))+'\n')
 		curr_contrib = contrib 
 		curr_dest = dest
 		
 rank = 0.15 + 0.85*curr_contrib
-print(curr_dest,round(rank,2),sep=",")
+#print(curr_dest,round(rank,2),sep=",")
+f.write(curr_dest+','+str(round(rank,2))+'\n')
+f.close()
 
