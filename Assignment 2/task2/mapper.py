1187172df5ae6e4d3d08f2bda0b5556abf7f4321
Ananya Uppal <55040997+ananya2712@users.noreply.github.com>
2021-10-22 20:08:11 +0530

    Add files via upload

--- Assignment 2/task2/mapper.py
+++ Assignment 2/task2/mapper.py
@@ -24,12 +24,10 @@ def dot(v1, v2):
 	return sum(x*y for x, y in zip(v1, v2))/(nom_v1*nom_v2)
 
 page_ranks = dict()
-for line in v_file.readlines():
-	line = line.strip().split(',')
-	page_ranks[int(line[0])] = int(line[1])
+for l in v_file.readlines():
+	l = l.strip().split(',')
+	page_ranks[int(l[0])] = int(l[1].strip())
 	
-# print(page_ranks)
-
 for line in input_m2:
 	page_no, adj_list = line.split('\t')
 	adj_list = adj_list.replace('[','')
@@ -40,16 +38,16 @@ for line in input_m2:
 	adj_list = adj_list.replace(' ','')
 	adj_list = adj_list.split(',')
 	
-	print(page_no,0)
+	page_no = page_no.replace(' ','')
+	page_no = int(page_no)
+	print(page_no,0,sep=',')
+	
 	for i in range(0, len(adj_list)):
 		adj_list[i] = int(adj_list[i])
 	
 	len_adj = len(adj_list)
 	if len(adj_list) == 0:
 		continue
-		
-	page_no = page_no.replace(' ','')
-	page_no = int(page_no)
 	try:
 		contribution = (page_ranks[page_no]/len(adj_list))
 	except KeyError: 
@@ -59,6 +57,7 @@ for line in input_m2:
 	
 	for i in adj_list:
 		sim = dot(data[str(page_no)],data[str(i)])
-		print(i,contribution*sim)
+		c = contribution*sim*page_ranks[page_no]
+		print(i,c,sep=',')
 embedding.close()
 v_file.close()
--- Assignment 2/task2/reducer.py
+++ Assignment 2/task2/reducer.py
@@ -6,28 +6,27 @@
 
 import sys
 
-filename = sys.argv[1]
-f = open(filename,'a')
-f.truncate(0)
+#filename = sys.argv[1]
+#f = open(filename,'a')
+#f.truncate(0)
 
 curr_dest = None
 curr_contrib = 0.0
 
 for line in sys.stdin:
-	dest,contrib = line.strip().split()
+	line = line.strip('\n').strip()
+	dest , contrib = line.split(',')
 	contrib = float(contrib)
 	if curr_dest is not None and curr_dest == dest:
 		curr_contrib += contrib 
 	else:
 		if curr_dest is not None:
 			rank = 0.15 + 0.85*curr_contrib
-			#print(curr_dest,round(rank,2),sep=",")
-			f.write(curr_dest+','+str(round(rank,2))+'\n')
+			print(curr_dest,round(rank,2),sep=",")
 		curr_contrib = contrib 
 		curr_dest = dest
 		
 rank = 0.15 + 0.85*curr_contrib
-#print(curr_dest,round(rank,2),sep=",")
-f.write(curr_dest+','+str(round(rank,2))+'\n')
-f.close()
+print(curr_dest,round(rank,2),sep=",")
+
 
