88227a22b8d383defbd5ba2b1c96e009a8ec2ff0
Kartika <56603537+kartika-nair@users.noreply.github.com>
2021-11-01 15:31:06 +0530

    t1 returns list of dicts

--- Assignment 3/task1.py
+++ Assignment 3/task1.py
@@ -1,7 +1,5 @@
 # Find the number of times where a city’s average temperature on a day turned out to be higher than the city’s average temperature throughout the dataset for a given country.
 from pyspark.sql import SparkSession
-# from pyspark.sql.functions import col
-# import org.apache.spark.sql.functions._
 import sys
 import pyspark.sql.functions as f
 
@@ -9,17 +7,11 @@ country = sys.argv[1]
 path = sys.argv[2]
 
 spark = SparkSession.builder.master("local[1]").appName("SparkByExamples.com").getOrCreate()
-
 city_file = spark.read.csv(path, inferSchema = True, header = True)
 
 if "Country" in city_file.columns:
-	avg_temp = city_file.where(f.col("Country") == country).groupBy("City").agg(f.avg("AverageTemperature")).collect()
-	d = avg_temp[0].asDict()
-	avg_dict = [{r['City']: r['avg(AverageTemperature)']} for r in avg_temp]
-	# print(avg_dict)
-
-c = city_file.where(f.col("Country") == country)
-greater = c.agg(f.count(f.when(f.col("AverageTemperature")> next(item for item in avg_dict if item["City"] == f.col("City"))))).show()
-print(greater)
-		
-
+	avg_temp = city_file.where(f.col("Country") == country).groupBy("City").agg(f.avg("AverageTemperature"))
+	rdd1 = avg_temp.rdd.map(lambda x: {x[0]: x[1]})
+	
+	print(avg_temp)
+	
