c8fc0f4fb1e851f544327a58535dad29a1d7eabe
Kartika <56603537+kartika-nair@users.noreply.github.com>
2021-11-02 15:50:55 +0530

    final a3 t1

--- Assignment 3/task1.py
+++ Assignment 3/task1.py
@@ -1,17 +1,24 @@
 # Find the number of times where a city’s average temperature on a day turned out to be higher than the city’s average temperature throughout the dataset for a given country.
+
 from pyspark.sql import SparkSession
 import sys
 import pyspark.sql.functions as f
+# import org.apache.spark.sql.functions._
 
 country = sys.argv[1]
 path = sys.argv[2]
 
-spark = SparkSession.builder.master("local[1]").appName("SparkByExamples.com").getOrCreate()
+spark = SparkSession.builder.master("local").appName("task1").getOrCreate()
 city_file = spark.read.csv(path, inferSchema = True, header = True)
 
 if "Country" in city_file.columns:
 	avg_temp = city_file.where(f.col("Country") == country).groupBy("City").agg(f.avg("AverageTemperature"))
-	rdd1 = avg_temp.rdd.map(lambda x: {x[0]: x[1]})
+	avg_temp = avg_temp.withColumnRenamed("avg(AverageTemperature)","average")
+	country = city_file.where(f.col("Country") == country)
 	
-	print(avg_temp)
+	ans = country.join(avg_temp,[ country.AverageTemperature > avg_temp.average , country.City == avg_temp.City ])
+	# print(ans.groupBy(country.City).count().collect())
 	
+	collectedDF = ans.groupBy(country.City).count().orderBy(f.asc(country.City)).collect()
+	for row in collectedDF:
+		print(row['City'] + '\t' + str(row['count']))
