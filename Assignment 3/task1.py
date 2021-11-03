eeabfb5178a8f0cc339f8e2f0ed3b12cf0f13ad4
Ananya Uppal <55040997+ananya2712@users.noreply.github.com>
2021-11-03 12:35:18 +0530

    Add files via upload

--- Assignment 3/task1.py
+++ Assignment 3/task1.py
@@ -1,13 +1,12 @@
-# Find the number of times where a city’s average temperature on a day turned out to be higher than the city’s average temperature throughout the dataset for a given country.
-
 from pyspark.sql import SparkSession
+from pyspark import SparkContext
 import sys
 import pyspark.sql.functions as f
-# import org.apache.spark.sql.functions._
 
 country = sys.argv[1]
 path = sys.argv[2]
 
+spark_context = SparkContext.getOrCreate()
 spark = SparkSession.builder.master("local").appName("task1").getOrCreate()
 city_file = spark.read.csv(path, inferSchema = True, header = True)
 
@@ -17,7 +16,6 @@ if "Country" in city_file.columns:
 	country = city_file.where(f.col("Country") == country)
 	
 	ans = country.join(avg_temp,[ country.AverageTemperature > avg_temp.average , country.City == avg_temp.City ])
-	# print(ans.groupBy(country.City).count().collect())
 	
 	collectedDF = ans.groupBy(country.City).count().orderBy(f.asc(country.City)).collect()
 	for row in collectedDF:
