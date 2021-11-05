fa0573a67d9881d44a23671c944034043e9891e8
Kartika <56603537+kartika-nair@users.noreply.github.com>
2021-11-05 18:21:14 +0530

    failing 2nd hidden test case

--- Assignment 3/task2.py
+++ Assignment 3/task2.py
@@ -16,13 +16,17 @@ global_file = spark.read.csv(globalPath, inferSchema = True, header = True)
 city_file.AverageTemperature = city_file.AverageTemperature.cast(FloatType())
 global_file.LandAverageTemperature = global_file.LandAverageTemperature.cast(FloatType())
 
-columns = ['dt', 'Country']
+columns = ['dt','Country']
+
 avg_temp_country = city_file.groupBy(columns).agg(f.max("AverageTemperature"))
 avg_temp_country = avg_temp_country.withColumnRenamed("max(AverageTemperature)","average")
 
 ans = avg_temp_country.join(global_file,[ avg_temp_country.average > global_file.LandAverageTemperature , avg_temp_country.dt == global_file.dt ])
+
+ans2 = ans.filter(avg_temp_country['average'] > global_file['LandAverageTemperature'])
 	
-collectedDF = ans.groupBy(avg_temp_country.Country).count().orderBy(f.asc(avg_temp_country.Country)).collect()
+collectedDF = ans2.groupBy(avg_temp_country.Country).count().orderBy(f.asc(avg_temp_country.Country)).collect()
 
 for row in collectedDF:
 	print(row['Country'] + '\t' + str(row['count']))
+
