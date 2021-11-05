bb9ff001124155e3b11c7a2cbf5939d80e310fd1
Kartika <56603537+kartika-nair@users.noreply.github.com>
2021-11-05 12:36:35 +0530

    Extra count for Finland, Germany, Poland, Turkmenistan

--- Assignment 3/task2.py
+++ Assignment 3/task2.py
@@ -1,13 +1,13 @@
-# Find the number of times where a country’s maximum average temperature on a date turned out to be higher than the worldwide land average temperature on the same date.
-
 from pyspark.sql import SparkSession
 import sys
 import pyspark.sql.functions as f
-# import org.apache.spark.sql.functions._
+from pyspark import SparkContext
 from pyspark.sql.types import FloatType
 
 cityPath = sys.argv[1]
 globalPath = sys.argv[2]
+
+spark_context = SparkContext.getOrCreate()
 
 spark = SparkSession.builder.master("local").appName("task1").getOrCreate()
 city_file = spark.read.csv(cityPath, inferSchema = True, header = True)
@@ -17,8 +17,8 @@ city_file.AverageTemperature = city_file.AverageTemperature.cast(FloatType())
 global_file.LandAverageTemperature = global_file.LandAverageTemperature.cast(FloatType())
 
 columns = ['dt', 'Country']
-avg_temp_country = city_file.groupBy(columns).agg(f.avg("AverageTemperature"))
-avg_temp_country = avg_temp_country.withColumnRenamed("avg(AverageTemperature)","average")
+avg_temp_country = city_file.groupBy(columns).agg(f.max("AverageTemperature"))
+avg_temp_country = avg_temp_country.withColumnRenamed("max(AverageTemperature)","average")
 
 ans = avg_temp_country.join(global_file,[ avg_temp_country.average > global_file.LandAverageTemperature , avg_temp_country.dt == global_file.dt ])
 	
@@ -26,4 +26,3 @@ collectedDF = ans.groupBy(avg_temp_country.Country).count().orderBy(f.asc(avg_te
 
 for row in collectedDF:
 	print(row['Country'] + '\t' + str(row['count']))
-
