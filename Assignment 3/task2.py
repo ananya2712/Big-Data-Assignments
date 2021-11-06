5422655a270307f728f44272d8e4b73bd2daa832
Kartika <56603537+kartika-nair@users.noreply.github.com>
2021-11-06 11:56:00 +0530

    dealt with null and na

--- Assignment 3/task2.py
+++ Assignment 3/task2.py
@@ -16,14 +16,20 @@ global_file = spark.read.csv(globalPath, inferSchema = True, header = True)
 city_file.AverageTemperature = city_file.AverageTemperature.cast(FloatType())
 global_file.LandAverageTemperature = global_file.LandAverageTemperature.cast(FloatType())
 
+cityNull = city_file.filter(city_file.AverageTemperature.isNotNull())
+globalNull = global_file.filter(global_file.LandAverageTemperature.isNotNull())
+
+cityNull.na.drop()
+globalNull.na.drop()
+
 columns = ['dt','Country']
 
-avg_temp_country = city_file.groupBy(columns).agg(f.max("AverageTemperature"))
+avg_temp_country = cityNull.groupBy(columns).agg({'AverageTemperature':'max'})
 avg_temp_country = avg_temp_country.withColumnRenamed("max(AverageTemperature)","average")
 
-ans = avg_temp_country.join(global_file,[ avg_temp_country.average > global_file.LandAverageTemperature , avg_temp_country.dt == global_file.dt ])
+ans = avg_temp_country.join(globalNull,[ avg_temp_country.average > globalNull.LandAverageTemperature , avg_temp_country.dt == globalNull.dt ])
 
-ans2 = ans.filter(avg_temp_country['average'] > global_file['LandAverageTemperature'])
+ans2 = ans.filter(avg_temp_country['average'] > globalNull['LandAverageTemperature'])
 	
 collectedDF = ans2.groupBy(avg_temp_country.Country).count().orderBy(f.asc(avg_temp_country.Country)).collect()
 
